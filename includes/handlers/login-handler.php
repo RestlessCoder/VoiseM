b6240be37e56b7155202605891a8d8c90f69bb85
RestlessCoder <nick.limca@hotmail.com>
2018-03-09 17:31:44 +0000

    Seperate our code for login & register handler

---
+++ includes/handlers/login-handler.php
@@ -0,0 +1,5 @@
+<?php
+	if(isset($_POST['loginButton'])) {
+		//Login button was pressed 
+	}
+?>
--- register.php
+++ register.php
@@ -1,38 +1,6 @@
 <?php 
-
-	function sanitazeFormUsername($inputText) {
-		$inputText = strip_tags($inputText);
-		$username = str_replace(" ", "", $inputText);
-		return $inputText;
-	}
-
-	function sanitazeFormPassword($inputText) {		
-		$inputText = strip_tags($inputText);
-		$username = str_replace(" ", "", $inputText);
-		return $inputText;
-	}
-
-	function sanitazeFormString($inputText) {
-		$firstName = strip_tags($firstName);
-		$firstName = str_replace(" ", "", $firstName);
-		$firstName = ucfirst(strtolower($firstName)); //Convert all the string to lower case first then convert first character to uppercase
-		return $inputText;
-	}
-	
-	if(isset($_POST['loginButton'])) {
-		//Login button was pressed 
-	}
-
-	if(isset($_POST['registerButton'])) {
-		//Register Button was pressed
-		$username = sanitazeFormUsername($_POST['username']);
-		$firstName = sanitazeFormString($_POST['firstName']);
-		$lastName = sanitazeFormString($_POST['lastName']);
-		$email = sanitazeFormString($_POST['email']);
-		$email2 = sanitazeFormString($_POST['email2']);
-		$password = sanitazeFormPassword($_POST['password']);
-		$password2 = sanitazeFormPassword($_POST['password2']);
-	}
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
 ?>
 
 <!DOCTYPE HTML>

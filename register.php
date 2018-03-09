74982ac603a0db3b2440deb00fa9b90bc2b76652
RestlessCoder <nick.limca@hotmail.com>
2018-03-09 17:24:10 +0000

    Add sanitize for cleaning the input

--- register.php
+++ register.php
@@ -1,3 +1,40 @@
+<?php 
+
+	function sanitazeFormUsername($inputText) {
+		$inputText = strip_tags($inputText);
+		$username = str_replace(" ", "", $inputText);
+		return $inputText;
+	}
+
+	function sanitazeFormPassword($inputText) {		
+		$inputText = strip_tags($inputText);
+		$username = str_replace(" ", "", $inputText);
+		return $inputText;
+	}
+
+	function sanitazeFormString($inputText) {
+		$firstName = strip_tags($firstName);
+		$firstName = str_replace(" ", "", $firstName);
+		$firstName = ucfirst(strtolower($firstName)); //Convert all the string to lower case first then convert first character to uppercase
+		return $inputText;
+	}
+	
+	if(isset($_POST['loginButton'])) {
+		//Login button was pressed 
+	}
+
+	if(isset($_POST['registerButton'])) {
+		//Register Button was pressed
+		$username = sanitazeFormUsername($_POST['username']);
+		$firstName = sanitazeFormString($_POST['firstName']);
+		$lastName = sanitazeFormString($_POST['lastName']);
+		$email = sanitazeFormString($_POST['email']);
+		$email2 = sanitazeFormString($_POST['email2']);
+		$password = sanitazeFormPassword($_POST['password']);
+		$password2 = sanitazeFormPassword($_POST['password2']);
+	}
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>

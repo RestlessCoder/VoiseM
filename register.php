a7fa5f99e7e2ff41107bd29e24bae7840abbfb3a
RestlessCoder <nick.limca@hotmail.com>
2018-03-11 22:52:15 +0000

    Add to remembering the form value

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php 
+	include("includes/config.php");
 	include("includes/classes/Account.php");
 
 	//Create a new instance of Account object into $account
@@ -6,6 +7,12 @@
 
 	include("includes/handlers/register-handler.php");
 	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
 ?>
 
 <!DOCTYPE HTML>
@@ -42,31 +49,31 @@
 		<p>	
 			<?php echo $account->getError("Your username must be 5 to 25 characters"); ?>
 			<label for="username">Username</label>
-			<input id="username" name="username" type="text" placeholder="e.g John123" required></input>
+			<input id="username" name="username" type="text" placeholder="e.g John123" value="<?php getInputValue('username') ?>" required></input>
 		</p>
 
 		<p>	
 			<?php echo $account->getError("Your first name must be 2 to 25 characters"); ?>
 			<label for="firstName">First Name</label>
-			<input id="firstName" name="firstName" type="text" placeholder="e.g John" required></input>
+			<input id="firstName" name="firstName" type="text" placeholder="e.g John" value="<?php getInputValue('firstName') ?>" required></input>
 		</p>
 
 		<p>	
 			<?php echo $account->getError("Your last name must be 2 to 25 characters"); ?>
 			<label for="lastName">Last Name</label>
-			<input id="lastName" name="lastName" type="text" placeholder="e.g John Cena" required></input>
+			<input id="lastName" name="lastName" type="text" placeholder="e.g John Cena" value="<?php getInputValue('lastName') ?>" required></input>
 		</p>
 
 		<p>	
 			<?php echo $account->getError("Your email don't match"); ?>
 			<?php echo $account->getError("Your email invalid"); ?>
 			<label for="email">Email</label>
-			<input id="email" name="email" type="email" placeholder="e.g John@hotmail.com" required></input>
+			<input id="email" name="email" type="email" placeholder="e.g John@hotmail.com" value="<?php getInputValue('email') ?>" required></input>
 		</p>
 
 		<p>
 			<label for="email2">Confirm email</label>
-			<input id="email2" name="email2" type="email" placeholder="e.g John@hotmail.com" required></input>
+			<input id="email2" name="email2" type="email" placeholder="e.g John@hotmail.com" value="<?php getInputValue('email2') ?>" required></input>
 		</p>
 		
 		<p>	
@@ -74,12 +81,12 @@
 			<?php echo $account->getError("Your passwords don't match"); ?>
 			<?php echo $account->getError("Your password must be 5 to 25 characters"); ?>
 			<label for="password">Password</label>
-			<input id="password" name="password" type="password" placeholder="Your password" required></input>
+			<input id="password" name="password" type="password" placeholder="Your password" value="<?php getInputValue('password') ?>" required></input>
 		</p>
 
 			<p>
 			<label for="password2">Confirm password</label>
-			<input id="password2" name="password2" type="password" placeholder="Retype your password" required></input>
+			<input id="password2" name="password2" type="password" placeholder="Retype your password" value="<?php getInputValue('firstName2') ?>" required></input>
 		</p>
 
 		<button type="submit" name="registerButton">SIGN UP</button>
